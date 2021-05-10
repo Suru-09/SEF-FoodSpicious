f5e2907b4ebfe7049aca334194bbd820e3b08e59
suru <dragos.surugiu13@yahoo.com>
2021-05-10 21:15:19 +0300

    [FOOD-29] Added hashing for password and database credentials

---
+++ src/main/java/config/HashPassword.java
@@ -0,0 +1,30 @@
+package config;
+
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class HashPassword {
+    public static String getEncryptedPassword(String password, String salt) throws Exception {
+        String algorithm = "PBKDF2WithHmacSHA1";
+        int derivedKeyLength = 160;
+        int iterations = 10000;
+
+        byte[] saltBytes = Base64.getDecoder().decode(salt);
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, iterations, derivedKeyLength);
+        SecretKeyFactory f = SecretKeyFactory.getInstance(algorithm);
+
+        byte[] encBytes = f.generateSecret(spec).getEncoded();
+        return Base64.getEncoder().encodeToString(encBytes);
+    }
+
+    // Returns base64 encoded salt
+    public static String getNewSalt() throws Exception {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[8];
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+}
